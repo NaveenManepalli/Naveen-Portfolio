b017c1c9d49809b926bf250e9336d3d5bd5b150c
Naveen.B <86225990+NaveenManepalli@users.noreply.github.com>
2024-06-11 13:08:45 +0530

    Add files via upload

---
+++ responsive-portfolio/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, render_template
+import smtplib
+from email.mime.text import MIMEText
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit-form', methods=['POST'])
+def submit_form():
+    name = request.form['name']
+    email = request.form['email']
+    subject = request.form['subject']
+    user_dtls = request.form['user_dtls']
+    
+    # Here you can process the data, e.g., send an email
+    send_email(name, email, subject, user_dtls)
+    
+    return 'Form submitted successfully!'
+
+def send_email(name, email, subject, message):
+    to = "manepallinaveen10@gmail.com"  
+    msg = MIMEText(f"Name: {name}\nEmail: {email}\n\nMessage:\n{message}")
+    msg['Subject'] = subject
+    msg['From'] = email
+    msg['To'] = to
+    
+    
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587
+    smtp_user = 'manepallinaveen10@gmail.com'
+    smtp_password = 'lemv dtvy bnbz pobb'
+    
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(smtp_user, smtp_password)
+        server.sendmail(email, [to], msg.as_string())
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ responsive-portfolio/static/js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Naveen.B</span> Aspiring AI and Machine Learning Engineering enthusiast, and committed to launching a dynamic career in the field. Eager to engage in hands-on experiences and contribute to pioneering projects, driving innovation in the latest technologies. Specifically interested in data analytics and software development to build a career in the software sector."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
